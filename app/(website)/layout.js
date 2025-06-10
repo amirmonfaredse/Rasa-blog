bc2e1a824cd15977eb4eb7525354f86429b34af0
amirmonfaredse <monfared.dev@gmail.com>
2025-06-10 13:07:16 +0330

    Clarity Test - 6

--- app/(website)/layout.js
+++ app/(website)/layout.js
@@ -22,7 +22,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl" className={NotoFont.className}>
       <Head>
-        <LoadClarity />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+         ((function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "r51f3tkzdw");`,
+          }}
+        />
       </Head>
       <body className="flex flex-col bg-ghost-100  text-ghost-1000 min-h-screen relative overflow-x-hidden">
         <Header />
